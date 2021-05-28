9c0050e786185e28354b5a57295fbce298a6341a
tolga <t.yigir35@gmail.com>
2021-05-28 11:22:16 -0400

    I added Project section 1 and 2.

--- src/test/java/VytrackProject.java
+++ src/test/java/VytrackProject.java
@@ -1,6 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 class VytrackProject {
     public static void main(String[] args) {
-        System.out.println(" ");
+
+    }
+    public static void TruckDriver(String username1, String password1) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        // 1. Open Chrome browser
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Given I am on login page : https://qa3.vytrack.com/user/login
+        driver.get("https://qa3.vytrack.com/user/login");
 
     }
 }
