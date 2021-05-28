901bd8ee69cf98f54f1c1beac82a3625a8bb65e4
Jorge Flores <jorgeareszuma86@gmail.com>
2021-05-28 10:13:27 -0500

    if statements VytrackProject.

--- src/test/java/VytrackProject.java
+++ src/test/java/VytrackProject.java
@@ -2,5 +2,10 @@ class VytrackProject {
     public static void main(String[] args) {
         System.out.println(" ");
 
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Login Successful for User:" + username1 + "  password: " + password1);
+        } else {
+            System.out.println("Login Failed for User: "+username1 + "  password: " + password1);
+        }
     }
-}
+
