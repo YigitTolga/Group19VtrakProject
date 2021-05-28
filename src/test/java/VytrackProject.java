e2a188c17c9948d36ef3d68869b4170f1b09ecb4
Alihan Tamer <alihantamer01@gmail.com>
2021-05-28 11:25:28 -0400

    Added some new codes

--- src/test/java/VytrackProject.java
+++ src/test/java/VytrackProject.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -18,6 +20,16 @@ class VytrackProject {
 
         //2. Given I am on login page : https://qa3.vytrack.com/user/login
         driver.get("https://qa3.vytrack.com/user/login");
+
+        //3. When I enter valid Truck Driver User name : User154
+        WebElement username = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        username.click();
+        username.sendKeys(username1);
+
+        //5. And I enter valid Password for user User154 : UserUser123
+        WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        password.click();
+        password.sendKeys(password1);
 
     }
 }
