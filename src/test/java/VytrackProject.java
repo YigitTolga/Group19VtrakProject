bd65b1bf8d57b37b3ad24e756c806e1ede961a25
Alihan Tamer <alihantamer01@gmail.com>
2021-05-28 11:10:53 -0400

    Added some codes

--- src/test/java/VytrackProject.java
+++ src/test/java/VytrackProject.java
@@ -1,6 +1,17 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 class VytrackProject {
     public static void main(String[] args) {
         System.out.println(" ");
+        //3. When I enter valid Truck Driver User name : User154
+        WebElement username = driver.findElement(By.xpath("//input[@id='prependedInput']"));
+        username.click();
+        username.sendKeys(username1);
 
+        //5. And I enter valid Password for user User154 : UserUser123
+        WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        password.click();
+        password.sendKeys(password1);
     }
 }
