697c647534fd501beb41e7b5edfffe167fd8272f
tolga <t.yigir35@gmail.com>
2021-06-07 13:24:51 -0400

    Added Steps information.

--- src/test/java/FuelLogsAddEventTest.java
+++ src/test/java/FuelLogsAddEventTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,7 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class FuelLogsAddEventTest {
 
 
-WebDriver driver;
+    WebDriver driver;
 
     @BeforeClass
     public void setupMethod() throws InterruptedException {
@@ -22,50 +23,77 @@ WebDriver driver;
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://qa3.vytrack.com/user/login");
     }
-        @Test
+    @AfterClass
+    public void quit() throws InterruptedException {
+        Thread.sleep(8000);
+        driver.quit();
+    }
+
+
     public void login() throws InterruptedException {
 
-        //3. When I enter valid user name:
+        //1. When I enter valid user name:
         WebElement username = driver.findElement(By.xpath("//input[@id='prependedInput']"));
         username.click();
         username.sendKeys("User153");
         Thread.sleep(1000);
-        //4. And I enter valid Password
-            WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
-            password.click();
-            password.sendKeys("UserUser123");
-            Thread.sleep(1000);
-            //5. And Click Login button
-            WebElement loginButton = driver.findElement(By.xpath("//button[@id='_submit']"));
-            loginButton.click();
-            Thread.sleep(1000);
-            driver.navigate().refresh();
-            WebElement fleet = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
-            fleet.click();
-            Thread.sleep(1000);
-            WebElement fuelLog = driver.findElement(By.xpath("(//span[@class='title title-level-2'])[5]"));
-            fuelLog.click();
-            Thread.sleep(1000);
-            WebElement viewLog = driver.findElement(By.xpath("//td[.='Exxon']"));
-            viewLog.click();
-            Thread.sleep(1000);
+        //2. And I enter valid Password
+        WebElement password = driver.findElement(By.xpath("//input[@id='prependedInput2']"));
+        password.click();
+        password.sendKeys("UserUser123");
+        Thread.sleep(1000);
+        //3. And Click Login button
+        WebElement loginButton = driver.findElement(By.xpath("//button[@id='_submit']"));
+        loginButton.click();
+        Thread.sleep(1000);
+        driver.navigate().refresh();
+    }
+    @Test
+    public void addEvent() throws InterruptedException {
+        //1.Login application
+        login();
+        //2. Click Fleet module on main page
+        WebElement fleet = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
+        fleet.click();
+        Thread.sleep(1000);
 
-            WebElement addEventBtn = driver.findElement(By.xpath("(//a[@class='btn icons-holder-text no-hash'])[2]"));
-            addEventBtn.click();
-            WebElement addEventTitle = driver.findElement(By.xpath("//input[@data-name='field__title']"));
-            addEventTitle.sendKeys("Receipt");
-            Thread.sleep(2000);
-            WebElement organizerName = driver.findElement(By.xpath("//input[@data-name='field__organizer-display-name']"));
-            organizerName.sendKeys("Tolga");
-            Thread.sleep(2000);
+        //3. Then click Fuel Log button
+        WebElement fuelLog = driver.findElement(By.xpath("(//span[@class='title title-level-2'])[5]"));
+        fuelLog.click();
+        Thread.sleep(2000);
 
-            WebElement organizerEmail = driver.findElement(By.xpath("//input[@data-name='field__organizer-email']"));
-            organizerEmail.sendKeys("Tolga@Tolga.com");
-            Thread.sleep(2000);
-            WebElement summitBtn = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
-            summitBtn.click();
-            driver.findElement(By.xpath("//a[.='Activity']")).click();
-        }
+        //4. Click on of fuel long and see view details
+        WebElement viewLog = driver.findElement(By.xpath("//td[.='Exxon']"));
+        viewLog.click();
+        Thread.sleep(2000);
+
+        //5. Click Add Event button
+                WebElement addEventBtn = driver.findElement(By.xpath("(//a[@class='btn icons-holder-text no-hash'])[2]"));
+        addEventBtn.click();
+        Thread.sleep(2000);
+
+        //6. Fill out required fields
+                //1. Fill title of event
+        WebElement addEventTitle = driver.findElement(By.xpath("//input[@data-name='field__title']"));
+        addEventTitle.sendKeys("Receipt");
+        Thread.sleep(2000);
+                //2. Fill Organizer name
+        WebElement organizerName = driver.findElement(By.xpath("//input[@data-name='field__organizer-display-name']"));
+        organizerName.sendKeys("Tolga");
+        Thread.sleep(2000);
+                //3. Fill Organizer Email address
+        WebElement organizerEmail = driver.findElement(By.xpath("//input[@data-name='field__organizer-email']"));
+        organizerEmail.sendKeys("Tolga@Tolga.com");
+        Thread.sleep(2000);
+
+        //7. Summit Event
+        WebElement summitBtn = driver.findElement(By.xpath("//button[@class='btn btn-primary']"));
+        summitBtn.click();
+        Thread.sleep(2000);
+
+        //8. Click activity button
+        driver.findElement(By.xpath("//a[.='Activity']")).click();
+    }
 
 
 }
