bad8bb546d80a5df6e8156bd7ede9810585cc62c
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-06-28 13:12:12 -0400

    Zebib and Gorge, Click Export Grid dropdown

--- src/test/java/Assignment_ZG.java
+++ src/test/java/Assignment_ZG.java
@@ -1,49 +1,67 @@
 import com.sun.tools.internal.xjc.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import utility.WebDriverFactory;
 
+import java.util.concurrent.TimeUnit;
 
 
 public class Assignment_ZG {
 
 
+    WebDriver driver;
+
+    @BeforeClass
+    public void setupMethod() throws InterruptedException {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.get("https://qa3.vytrack.com/user/login");
+    }
+
+
     @Test
-    public void VyTrack_test1() {
+    public void VyTrack_test1() throws InterruptedException {
 
         //1. Open Chrome browser
-        Driver.getDriver().get("https://qa3.vytrack.com/user/login");
+
+        driver.get("https://qa3.vytrack.com/user/login");
 
         //2. Enter valid user name (user154)
-        WebElement userName = Driver.getDriver().findElement(By.xpath("//*[@id='prependedInput']"));
+        WebElement userName = driver.findElement(By.xpath("//*[@id='prependedInput']"));
         userName.sendKeys("user154");
-        BrowserUtils.sleep(2);
+        Thread.sleep(2);
 
 
         //3. Enter valid Password (UserUser123)
-        WebElement password = Driver.getDriver().findElement(By.xpath("//*[@id='prependedInput2']"));
+        WebElement password = driver.findElement(By.xpath("//*[@id='prependedInput2']"));
         password.sendKeys("UserUser123");
-        BrowserUtils.sleep(2);
+        Thread.sleep(2);
 
 
         //4. Click Login button
-        WebElement clickLoginButton = Driver.getDriver().findElement(By.xpath("//*[@id='_submit']"));
+        WebElement clickLoginButton =driver.findElement(By.xpath("//*[@id='_submit']"));
         clickLoginButton.click();
-        BrowserUtils.sleep(2);
+        Thread.sleep(2);
 
         //5. Navigate Fleet module
-        WebElement fleetDroDown = Driver.getDriver().findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
+        WebElement fleetDroDown = driver.findElement(By.xpath("(//span[@class='title title-level-1'])[1]"));
         fleetDroDown.click();
-        BrowserUtils.sleep(3);
+        Thread.sleep(2);
 
         //6. Select Vehicle under Fleet module
-        WebElement vehicle  = Driver.getDriver().findElement(By.xpath("(//*[@class='title title-level-2'])[1]"));
+        WebElement vehicle  = driver.findElement(By.xpath("(//*[@class='title title-level-2'])[1]"));
         vehicle.click();
-        BrowserUtils.sleep(2);
+        Thread.sleep(2);
 
         //6. Click Export Grid dropdown
-        WebElement exportGrid = Driver.getDriver().findElement(By.xpath("(//a[@data-toggle = 'dropdown'])[4]"));
+        WebElement exportGrid = driver.findElement(By.xpath("(//a[@data-toggle = 'dropdown'])[4]"));
         exportGrid.click();
 
         // 7. Verify text displayed is as expected:
